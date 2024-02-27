74f18d9445b6aad9a4f6861bea5855f050fa064f
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2024-02-27 23:23:48 +0500

    Add components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,1 +1,242 @@
-export { Button, type ButtonProps } from "./button";
+export {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "./components/ui/accordion"
+  export { Alert, AlertDescription, AlertTitle } from "./components/ui/alert"
+  export { cn } from "./lib/utils"
+  export {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "./components/ui/alert-dialog"
+  export { AspectRatio } from "./components/ui/aspect-ratio"
+  export { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar"
+  
+  export { Badge, badgeVariants } from "./components/ui/badge"
+  export {
+    Button,
+    type ButtonProps,
+    buttonVariants,
+  } from "./components/ui/button"
+
+  export { Calendar } from "./components/ui/calendar"
+  export { useToast, toast } from "./components/ui/use-toast"
+  export type { ToastActionElement, ToastProps } from "./components/ui/toast"
+  export {
+    Toast,
+    ToastProvider,
+    ToastViewport,
+    ToastTitle,
+    ToastDescription,
+    ToastClose,
+    ToastAction,
+  } from "./components/ui/toast"
+  export { Checkbox } from "./components/ui/checkbox"
+  export {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "./components/ui/card"
+  export {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "./components/ui/carousel"
+  export {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+  } from "./components/ui/collapsible"
+  export {
+    Command,
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+  } from "./components/ui/command"
+  export {
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+  } from "./components/ui/context-menu"
+  
+  export {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+  } from "./components/ui/dialog"
+  
+  export {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "./components/ui/drawer"
+  
+  export {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuPortal,
+    DropdownMenuGroup,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+  } from "./components/ui/dropdown-menu"
+  export type { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+  export {
+    useFormField,
+    Form,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormDescription,
+    FormMessage,
+    FormField,
+  } from "./components/ui/form"
+  
+  export {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+  } from "./components/ui/hover-card"
+  export { Input } from "./components/ui/input"
+  export { Label } from "./components/ui/label"
+  export {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+  } from "./components/ui/menubar"
+  export {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+    navigationMenuTriggerStyle,
+  } from "./components/ui/navigation-menu"
+  
+  export {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "./components/ui/pagination"
+  export {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "./components/ui/popover"
+  export { Progress } from "./components/ui/progress"
+  export { RadioGroup, RadioGroupItem } from "./components/ui/radio-group"
+  export {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "./components/ui/resizable"
+  export { ScrollArea, ScrollBar } from "./components/ui/scroll-area"
+  export {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectGroup,
+    SelectLabel,
+  } from "./components/ui/select"
+  export { Separator } from "./components/ui/separator"
+  export {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+    SheetClose,
+    SheetFooter,
+  } from "./components/ui/sheet"
+  export { Skeleton } from "./components/ui/skeleton"
+  export { Slider } from "./components/ui/slider"
+  export { Toaster } from "./components/ui/toaster"
+  export { Toaster as SonnerToaster } from "./components/ui/sonner"
+  export { Switch } from "./components/ui/switch"
+  export {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+    TableFooter,
+  } from "./components/ui/table"
+  export { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
+  export { Textarea } from "./components/ui/textarea"
+  export { Toggle } from "./components/ui/toggle"
+  export { ToggleGroup, ToggleGroupItem } from "./components/ui/toggle-group"
+  export {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "./components/ui/tooltip"
+  // export { RichTextEditor } from "./components/ui/rich-texteditor"
+  
+  // polygon identity or
+  // unlock locksmith api sign in with ethereum and you post a message to the api and it will sign it with your private key and then you can use that to sign in to the api
+  
